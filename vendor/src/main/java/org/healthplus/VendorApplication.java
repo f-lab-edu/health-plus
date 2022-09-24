3b2f4a026b04092d69a7904454cc0d61c32a72a5
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-09-24 14:01:56 +0900

    fix : springbootapplication

--- vendor/src/main/java/org/healthplus/VendorApplication.java
+++ vendor/src/main/java/org/healthplus/VendorApplication.java
@@ -1,13 +1,9 @@
 package org.healthplus;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class VendorApplication {
     public static void main(String[] args) {
         SpringApplication.run(VendorApplication.class, args);
