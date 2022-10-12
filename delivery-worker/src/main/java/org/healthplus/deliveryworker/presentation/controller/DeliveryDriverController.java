86660840ce2fa3f54f8f7d7afb708bc806f33038
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-10-12 21:49:13 +0900

    ADD : DB DDL files

--- delivery-worker/src/main/java/org/healthplus/deliveryworker/presentation/controller/DeliveryDriverController.java
+++ delivery-worker/src/main/java/org/healthplus/deliveryworker/presentation/controller/DeliveryDriverController.java
@@ -4,10 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.healthplus.deliveryworker.presentation.requestDto.DeliveryInfoDto;
 import org.healthplus.deliveryworker.service.DeliveryDriverService;
-import org.healthplus.deliveryworker.service.command.DeliveryInfoCommand;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +19,7 @@ public class DeliveryDriverController {
 
   private final DeliveryDriverService deliveryDriverService;
 
-  @
+  @ResponseStatus(HttpStatus.OK)
   @PostMapping("/signup")
   public String signUp(@RequestBody DeliveryInfoDto deliveryInfoDto) {
     log.info("deliveryInfoDto = {}", deliveryInfoDto.toString());
