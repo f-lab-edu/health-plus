bbd8772a785bd2b78336155a9159f5b08641933c
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-10-26 18:58:13 +0900

    add : db ddl v19

---
+++ app/src/main/resources/db/migration/V19__update_delivery_driver_table.sql
@@ -0,0 +1,1 @@
+ALTER TABLE delivery_driver ADD delivery_status CHAR(1) DEFAULT 'N' NOT NULL COMMENT '배달 가능 여부';
--- delivery-worker/src/main/java/org/healthplus/deliveryworker/enums/DeliveryStatus.java
+++ delivery-worker/src/main/java/org/healthplus/deliveryworker/enums/DeliveryStatus.java
@@ -1,5 +1,5 @@
 package org.healthplus.deliveryworker.enums;
 
 public enum DeliveryStatus {
-  CAN, CANNOT
+  Y, N
 }
