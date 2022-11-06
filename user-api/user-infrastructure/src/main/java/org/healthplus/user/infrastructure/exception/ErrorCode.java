19f5d21bf3aafa9a020c6b72b34e19d6319aeed6
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-11-06 16:41:47 +0900

    feat : jwt header exceptino check

--- user-api/user-infrastructure/src/main/java/org/healthplus/user/infrastructure/exception/ErrorCode.java
+++ user-api/user-infrastructure/src/main/java/org/healthplus/user/infrastructure/exception/ErrorCode.java
@@ -6,7 +6,10 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
-public class ErrorCode {
+public enum ErrorCode {
+
+  JWT_ALGORITHM_NAME_EXCEPTION(HttpStatus.NOT_ACCEPTABLE, "JWT 알고리즘을 정확히 해야합니다."),
+  TOKEN_TYPE_NAME_EXCEPTION(HttpStatus.NOT_ACCEPTABLE, "JWT Token 타입이 아닙니다.");
 
   private final HttpStatus httpStatus;
   private final String message;
--- user-api/user-infrastructure/src/main/java/org/healthplus/user/infrastructure/security/jwt/wrapper/TokenHeader.java
+++ user-api/user-infrastructure/src/main/java/org/healthplus/user/infrastructure/security/jwt/wrapper/TokenHeader.java
@@ -1,5 +1,9 @@
 package org.healthplus.user.infrastructure.security.jwt.wrapper;
 
+import static org.healthplus.user.infrastructure.exception.ErrorCode.JWT_ALGORITHM_NAME_EXCEPTION;
+import static org.healthplus.user.infrastructure.exception.ErrorCode.TOKEN_TYPE_NAME_EXCEPTION;
+
+import org.healthplus.user.infrastructure.exception.JwtException;
 import org.healthplus.user.infrastructure.security.jwt.enums.HeaderType;
 import org.healthplus.user.infrastructure.security.jwt.enums.SignatureAlgorithm;
 
@@ -9,11 +13,31 @@ public class TokenHeader {
   private HeaderType type;
 
   private TokenHeader(String algo, String type) {
+
+    isJwtAlgo(algo);
+    isJwtTokenType(type);
+
     this.algo = SignatureAlgorithm.valueOf(algo);
     this.type = HeaderType.valueOf(type);
   }
 
   public static TokenHeader of(String algo, String type) {
     return new TokenHeader(algo, type);
+  }
+
+  private void isJwtTokenType(String type) {
+    for (HeaderType header : HeaderType.values()) {
+      if (header.name() != type) {
+        throw new JwtException(TOKEN_TYPE_NAME_EXCEPTION);
+      }
+    }
+  }
+
+  private void isJwtAlgo(String algo) {
+    for (SignatureAlgorithm algorithm : SignatureAlgorithm.values()) {
+      if (algorithm.name() != algo) {
+        throw new JwtException(JWT_ALGORITHM_NAME_EXCEPTION);
+      }
+    }
   }
 }
