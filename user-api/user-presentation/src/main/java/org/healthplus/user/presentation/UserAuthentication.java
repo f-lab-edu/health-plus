2392030a976dde1dccc2ee5b543c242afec3ff5b
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-11-23 17:45:16 +0900

    refactor : user api presentation

--- user-api/user-presentation/src/main/java/org/healthplus/user/presentation/UserAuthentication.java
+++ user-api/user-presentation/src/main/java/org/healthplus/user/presentation/UserAuthentication.java
@@ -4,32 +4,47 @@ import lombok.RequiredArgsConstructor;
 import org.healthplus.user.application.UserService;
 import org.healthplus.user.presentation.request.UserLoginRequest;
 import org.healthplus.user.presentation.request.UserRegisterRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class UserAuthentication implements UserAuthenticationSpecification {
+public class UserAuthentication {
 
   private final UserService userService;
 
-  @Override
+  /*
+  * 회원 가입
+  * */
+  @PostMapping("/api/auth/user/register")
   public void register(UserRegisterRequest registerRequest) {
-    userService.signUp(registerRequest.getId(), registerRequest.getName(),
+    userService.signUp(registerRequest.getEmail(), registerRequest.getName(),
         registerRequest.getEmail(),
         registerRequest.getPassword(), registerRequest.getPhoneNumber());
   }
 
-  @Override
+
+  /*
+   * 회원 가입 확인
+   * */
+  @GetMapping("/api/auth/user/confirm")
   public void registerConfirm() {
 
   }
 
-  @Override
+  /*
+   * 회원 로그인
+   * */
+  @PostMapping("/api/auth/user/login")
   public void login(UserLoginRequest loginRequest) {
     userService.login(loginRequest.getId(), loginRequest.getPassword());
   }
 
-  @Override
+  /*
+  * refreshToken 재발급
+  * */
+  @PostMapping("/api/auth/user/reIssuance")
   public void reIssuance() {
 
   }
