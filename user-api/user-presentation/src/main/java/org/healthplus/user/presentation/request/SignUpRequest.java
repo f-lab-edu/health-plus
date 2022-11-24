873e77e34da4f2e05c0602d8061b6e9745f7a4b8
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-11-24 20:43:54 +0900

    feat : SignUpRequest Validation

--- user-api/user-presentation/src/main/java/org/healthplus/user/presentation/request/SignUpRequest.java
+++ user-api/user-presentation/src/main/java/org/healthplus/user/presentation/request/SignUpRequest.java
@@ -1,5 +1,8 @@
 package org.healthplus.user.presentation.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,11 +10,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignUpRequest {
 
+  @NotNull
+  @Email
   private String email;
 
-  private String name;
+  @NotNull
+  private String nickName;
 
+  @NotNull
+  @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+      message = "최소 8 자, 최소 하나의 문자 및 하나의 숫자를 입력해주세요.")
   private String password;
 
+  @NotNull
+  @Pattern(regexp = "^\\d{11}$", message = "'-'없이 숫자 11자리만 입력해주세요.")
   private String phoneNumber;
 }
