8a7117f795ad75257d284ee444f22df4730f1473
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-11-29 14:15:03 +0900

    chore : shop presentation module dependency change

--- shop-api/shop-presentation/src/main/java/org/healthplus/shop/presentation/MenuModificationController.java
+++ shop-api/shop-presentation/src/main/java/org/healthplus/shop/presentation/MenuModificationController.java
@@ -1,7 +1,6 @@
 package org.healthplus.shop.presentation;
 
 import lombok.RequiredArgsConstructor;
-import org.healthplus.shop.application.service.MenuModificationService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- shop-api/shop-presentation/src/main/java/org/healthplus/shop/presentation/MenuRemovalController.java
+++ shop-api/shop-presentation/src/main/java/org/healthplus/shop/presentation/MenuRemovalController.java
@@ -1,7 +1,6 @@
 package org.healthplus.shop.presentation;
 
 import lombok.RequiredArgsConstructor;
-import org.healthplus.shop.application.service.MenuRemovalService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/menu")
 public class MenuRemovalController {
 
-  // private final MenuRemovalService menuRemovalService;
+   // private final MenuRemovalService menuRemovalService;
 }
