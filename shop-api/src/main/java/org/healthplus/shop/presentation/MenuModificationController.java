36fb41888ae0c659cca567600337b3c43ecb1e54
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-11-30 22:27:51 +0900

    chore : change shop api and order api

---
+++ shop-api/src/main/java/org/healthplus/shop/presentation/MenuModificationController.java
@@ -0,0 +1,13 @@
+package org.healthplus.shop.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/menu")
+public class MenuModificationController {
+
+  // private final MenuModificationService menuModificationService;
+}
---
+++ shop-api/src/main/java/org/healthplus/shop/presentation/MenuRemovalController.java
@@ -0,0 +1,13 @@
+package org.healthplus.shop.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/menu")
+public class MenuRemovalController {
+
+   // private final MenuRemovalService menuRemovalService;
+}
