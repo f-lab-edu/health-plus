979f7afe9efc1776362cd213e662837a994dbe02
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-12-20 17:10:56 +0900

    feat : sessionAuthorization service

--- account-api/src/main/java/org/healthplus/account/application/SessionAuthorization.java
+++ account-api/src/main/java/org/healthplus/account/application/SessionAuthorization.java
@@ -1,7 +1,9 @@
 package org.healthplus.account.application;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.healthplus.account.application.command.AuthorizationCommand;
+import org.healthplus.account.application.result.AuthorizationResult;
 import org.healthplus.account.domain.Authorization;
 import org.healthplus.account.domain.vo.SessionValueVO;
 import org.springframework.stereotype.Component;
@@ -10,7 +12,7 @@ import org.springframework.stereotype.Component;
 /*
  * Authorization Interface를 이용해 의존성 역전 -> 확작성 있게 설계됨
  * */
- @Component
+@Component
 public class SessionAuthorization implements Authorization {
 
   private static final String HEALTH_PLUS_ACCOUNT_SESSION_VALUE = "HEALTH_PLUS_ACCOUNT_SESSION_VALUE";
@@ -30,5 +32,16 @@ public class SessionAuthorization implements Authorization {
     );
     httpSession.setAttribute(HEALTH_PLUS_ACCOUNT_SESSION_VALUE, sessionValueVO);
     return null;
+  }
+
+  @Override
+  public AuthorizationResult getCurrentUser(HttpServletRequest request) {
+    SessionValueVO sessionValue = (SessionValueVO) request.getSession()
+        .getAttribute(HEALTH_PLUS_ACCOUNT_SESSION_VALUE);
+
+    if (sessionValue == null) {
+      return null;
+    }
+    return AuthorizationResult.fromSessionValue(sessionValue);
   }
 }
